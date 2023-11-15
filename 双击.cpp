d01b9398af541e7829f22be320e03eb2fdaf94d5
yufao <145562731+yufao@users.noreply.github.com>
2023-11-15 19:55:09 +0800

    Add files via upload

---
+++ 双击.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h> 
+#include<vector>
+#include<windows.h>
+#include<iostream>
+#include<fstream>
+using namespace std;
+int T,i,k;
+char x[200008],y[200008];
+
+int n[40],m[40];
+int main()
+{ void guilei(char z[],char c[],int s[])      
+  vector<char>a(40);
+  vector<char>b(40);
+   
+   scanf("%d",&T);
+   for(int i=1;i<=T;i++)
+   { scanf("%s",x);
+     scanf("%s",y);
+     guilei(x,a,n);
+   }
+	
+}
+
+
+
+void guilei(char z[], char c[],int s[])
+{   int j,i,e,t,p,q;
+    t=0;
+    p=strlen(z)+1;
+    q=strlen(c);
+	for(int j=1;j<=p;j++)
+	{	
+	  { 
+	    if(z[j]==z[j+1])
+		{ 
+		};
+		if(z[j]!=z[j+e])
+		{  
+		};
+	  }
+	
+	}
+   for(int i=0;i<=1;i++){
+
+   }
+
+	
+}
